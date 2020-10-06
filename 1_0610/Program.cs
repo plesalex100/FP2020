331352e9b557815e9212f6b2c599025c6aed1aef
Ples Alex <plesalex100@gmail.com>
2020-10-06 11:08:47 +0300

    my aproach

--- 1_0610/Program.cs
+++ 1_0610/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace _1_0610
+﻿using System;
+/// <summary>
+/// Distanta pana la Stormwind
+/// </summary>
+namespace _1_0610
 {
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("Introduceti distanta la care sunteti pe autostrada");
+                int k = int.Parse(Console.ReadLine());
+
+                if (k > 60) k -= 60;
+                else k = 60 - k;
+
+                Console.WriteLine($"Va aflati la {k} km de Stormwind");
+
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
